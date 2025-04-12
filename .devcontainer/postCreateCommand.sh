3a6561a499f2e033a2c1c7f6c66974908941c4bb
asapoka <asapoka@outlook.com>
2025-04-12 15:35:11 +0000

    DockerfileにHomebrewとdotfilesのインストールを追加

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-# Update package lists
-sudo apt-get update
-
-# Install necessary packages
-sudo apt-get install -y cmake build-essential pkg-config
-
 # Any other setup commands you need
 
-#cargo install starship --locked 
-# cargo install lsd sheldon 
-# cargo install --locked bat
-# cargo install --locked starship
-# curl -sS https://starship.rs/install.sh | sh
 
-
+# install my dotfiles
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/asapoka/dotfiles/master/install.bash)"
-# Print a message indicating the script has finished
 echo "Post-create commands have been executed."
