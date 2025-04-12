5883ada190f9fa2f2498e86a09adcd85c4d069ad
asapoka <asapoka@outlook.com>
2025-04-13 00:47:35 +0900

    postCreateCommand.shのHomebrewインストールのコメントを修正し、dotfilesのインストールを明示化

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Any other setup commands you need
-# install brew 
+# install brew
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # install my dotfiles
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/asapoka/dotfiles/master/install.bash)"
+bash ~/dotfiles/install.sh
 echo "Post-create commands have been executed."
